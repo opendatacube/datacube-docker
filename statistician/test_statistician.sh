de6f6c54295592bd84d9d09110e1a65e4d8aba44
Alex Leith <alexgleith@gmail.com>
2020-10-15 13:05:06 +1100

    Add actual integration tests for stats

---
+++ statistician/test_statistician.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+docker-compose up -d
+docker-compose exec -t odc-stats --version
+echo "Indexing some data"
+docker-compose exec -T stats datacube system init
+docker-compose exec -T stats datacube system check
+docker-compose exec -T stats datacube product add https://raw.githubusercontent.com/digitalearthafrica/config/master/products/esa_s2_l2a.yaml
+docker-compose exec -T stats stac-to-dc --bbox='4,5,5,6' --collections='sentinel-s2-l2a-cogs' --datetime='2019-09-01/2020-01-01' s2_l2a
+echo "Checking save tasks"
+docker-compose exec -T stats odc-stats save-tasks --grid albers_africa_10 --year 2019 --tiles 5:8,5:8 --overwrite s2_l2a test-run.db
+echo "Checking a job run"
+docker-compose exec -T stats odc-stats run-pq --location file:///tmp ./test-run.db 0
+docker-compose exec -T stats ls /tmp
