b88eb6a92334635a8e8fc75eb354a80beca46434
Alex Leith <alexgleith@gmail.com>
2020-10-15 14:27:31 +1100

    Update tests and make them better (#4)
    * Update tests and make them better
    
    * Fix yaml issue
    
    * Change to no-sign-request
    
    * Fix yaml again, this time with a proper linter
    
    * Fix index docker compose file
    
    * Change stats command
    
    * Fix docker-compose for index
    
    * Update statistician test workflow
    
    * Run it online, why not
    
    * Add actual integration tests for stats
    
    * Fix up Statistician test script
    
    * Rename actions
    
    * Add wget to index image requirements
    
    * Add one more requirement back for indexing
    
    * Add quotes to the path things for github action selectors
    
    * Rename actions

--- README.md
+++ README.md
@@ -2,10 +2,16 @@
 
 General purpose Docker Images related to the Open Data Cube project
 
-## Statistician
+## How this works
 
-See: [statistician](statistician/readme.md).
+Each folder has a Dockerfile and a version.txt, as a minimum. There are two GitHub Actions
+pipelines per image, one to test and one to build. In order to rebuild an image and have
+it pushed to GitHub, you should increment the number in the version.txt file. Changing _anything_
+in the folder of an Image currently triggers a run too.
 
-## Index
+Each project should have a docker-compose file that can be used to run and test the image.
 
-See: [index](index/readme.md).
+## List of images
+
+* [opendatacube/datacube-statistician](statistician/readme.md): A minimal Image that is used to run Statistician processing
+* [opendatacube/datacube-index](index/readme.md): A suite of tools to manage indexing into an Open Data Cube database
---
+++ index/test_indexing.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+
+# Echo lines and fail fast
+set -ex
+
+docker-compose up -d
+docker-compose exec -T index datacube system init
+docker-compose exec -T index datacube system check
+echo "Checking InSAR indexing"
+docker-compose exec -T index datacube product add https://raw.githubusercontent.com/GeoscienceAustralia/digitalearthau/develop/digitalearthau/config/products/cemp_insar_alos_displacement.yaml
+docker-compose exec -T index s3-to-dc --no-sign-request "s3://dea-public-data/cemp_insar/insar/displacement/alos/2010/**/*.yaml" cemp_insar_alos_displacement
+echo "Checking Indexed Datasets Count"
+docker-compose exec -T postgres psql -U postgres -c "SELECT count(*) from agdc.dataset"
+echo "Checking STAC indexing"
+docker-compose exec -T index datacube product add https://raw.githubusercontent.com/digitalearthafrica/config/master/products/esa_s2_l2a.yaml
+docker-compose exec -T index s3-to-dc --no-sign-request --stac "s3://sentinel-cogs/sentinel-s2-l2a-cogs/2020/S2A_32NNF_20200127_0_L2A/*.json" s2_l2a
+echo "Checking STAC API indexing"
+docker-compose exec -T index stac-to-dc --bbox='5,15,10,20' --limit=10 --collections='sentinel-s2-l2a-cogs' --datetime='2020-08-01/2020-08-31' s2_l2a
+echo "Checking Indexed Datasets Count (including STAC)"
+docker-compose exec -T postgres psql -U postgres -c "SELECT count(*) from agdc.dataset"
+docker-compose down
