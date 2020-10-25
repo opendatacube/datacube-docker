d3c6ba110dd0efd92931f2726f5527673c5deb29
Toktam <toktam.ebadi@ga.gov.au>
2020-10-25 20:02:08 +1100

    Add utm-zone as input.

--- index/assets/index-sentinel-2-capetown.py
+++ index/assets/index-sentinel-2-capetown.py
@@ -8,16 +8,12 @@ import pandas as pd
 
 @click.command("index-sentinel-2_capetown")
 @click.argument("product", type=str, nargs=1)
+@click.argument("utm-zone", type=str, nargs=1)
 
-def cli(product):
-    region_code_list_uri="https://raw.githubusercontent.com/digitalearthafrica/deafrica-extent/master/deafrica-mgrs-tiles.csv.gz"
-    region_codes = pd.Series(pd.read_csv(region_code_list_uri).values.ravel())
-    utm_zone = set(region_codes.str[0:2])
-
-    for x in utm_zone:
-        s3_path = f"s3://deafrica-sentinel-2/sentinel-s2-l2a-cogs/{x}/**/*.json"
-        print(f"Running for {s3_path} and {product}")
-        subprocess.run(["s3-to-dc", '--stac', '--no-sign-request', s3_path, product])
+def cli(product, utm_zone):
+    s3_path = f"s3://deafrica-sentinel-2/sentinel-s2-l2a-cogs/{utm_zone}/**/*.json"
+    print(f"Running for {s3_path} and {product}")
+    subprocess.run(["s3-to-dc", '--stac', '--no-sign-request', s3_path, product])
 
 
 if __name__ == "__main__":
