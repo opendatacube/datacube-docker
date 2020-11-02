41672a60060931001372292b95328dd5994992e1
Alex Leith <alexgleith@gmail.com>
2020-11-02 12:13:42 +1100

    Fix up running so tests work locally

--- index/test_bootstrapping.sh
+++ index/test_bootstrapping.sh
@@ -4,7 +4,7 @@
 set -ex
 
 docker-compose up -d
-docker-compose run index /code/bootstrap-odc.sh $PRODUCT_CATALOG $METADATA_CATALOG
+docker-compose run index bash -c "/code/bootstrap-odc.sh \$PRODUCT_CATALOG \$METADATA_CATALOG"
 docker-compose exec -T postgres psql -U postgres -c "SELECT count(*) from agdc.metadata_type"
 docker-compose exec -T postgres psql -U postgres -c "SELECT count(*) from agdc.dataset_type"
 docker-compose down
