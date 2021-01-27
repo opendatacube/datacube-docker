1a80e8bbb901a28fcffc1698af6d38fa48c7b402
Kirill Kouzoubov <kirill888@gmail.com>
2021-01-27 20:08:20 +1100

    Update test: grid names have changed, no more run-pq

--- statistician/test_statistician.sh
+++ statistician/test_statistician.sh
@@ -11,7 +11,7 @@ docker-compose exec -T stats datacube system check
 docker-compose exec -T stats datacube product add https://raw.githubusercontent.com/digitalearthafrica/config/master/products/esa_s2_l2a.yaml
 docker-compose exec -T stats stac-to-dc --bbox='4,5,5,6' --collections='sentinel-s2-l2a-cogs' --datetime='2019-09-01/2020-01-01' s2_l2a
 echo "Checking save tasks"
-docker-compose exec -T stats odc-stats save-tasks --grid albers_africa_10 --year 2019 --tiles 5:8,5:8 --overwrite s2_l2a test-run.db
+docker-compose exec -T stats odc-stats save-tasks --grid africa-20 --year 2019 --tiles 5:8,5:8 --overwrite s2_l2a test-run.db
 echo "Checking a job run"
-docker-compose exec -T stats odc-stats run-pq --location file:///tmp ./test-run.db 0
+docker-compose exec -T stats odc-stats run --plugin pq --location file:///tmp ./test-run.db 0
 docker-compose exec -T stats ls /tmp
