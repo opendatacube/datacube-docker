56d2e2c3ad2be4931e98720336d84ab883bda850
Sai Ma <Sai.Ma@ga.gov.au>
2021-10-22 05:17:24 +0000

    Add FCP relative test

--- statistician/test_statistician.sh
+++ statistician/test_statistician.sh
@@ -14,7 +14,6 @@ echo "Checking save tasks"
 docker-compose exec -T stats odc-stats save-tasks --grid africa-20 --year 2019 --overwrite s2_l2a test-run.db
 echo "Checking a job run"
 docker-compose exec -T stats odc-stats run  --threads=1 --plugin pq --location file:///tmp ./test-run.db 0
-docker-compose exec -T stats ls /tmp
 
 # add eodatasets3 + OWS integration test
 
@@ -51,3 +50,21 @@ docker-compose exec -T stats odc-stats save-tasks --grid=au-30 --year=2015 --ove
 echo "Checking WO summary EODatasets3 integration job run"
 docker-compose exec -T stats wget https://raw.githubusercontent.com/GeoscienceAustralia/dea-config/master/dev/services/odc-stats/wofs_summary/ga_ls_wo_fq_cyear_3.yaml
 docker-compose exec -T stats odc-stats run  --threads=1 --config ga_ls_wo_fq_cyear_3.yaml --resolution=30 --location file:///tmp wo-summary-eo3-test-run.db 0 --apply_eodatasets3
+
+# 3) FCP-AU
+docker-compose exec -T stats datacube product add https://raw.githubusercontent.com/GeoscienceAustralia/dea-config/master/products/land_and_vegetation/c3_fc/ga_ls_fc_3.odc-product.yaml
+
+# only index several data to speed up yearly summary run
+docker-compose exec -e AWS_DEFAULT_REGION=ap-southeast-2 -T stats s3-to-dc "s3://dea-public-data/derivative/ga_ls_fc_3/2-5-0/088/079/2015/02/*/*.json" --no-sign-request --skip-lineage --stac ga_ls_fc_3
+docker-compose exec -e AWS_DEFAULT_REGION=ap-southeast-2 -T stats s3-to-dc "s3://dea-public-data/derivative/ga_ls_fc_3/2-5-0/088/079/2015/03/*/*.json" --no-sign-request --skip-lineage --stac ga_ls_fc_3
+docker-compose exec -e AWS_DEFAULT_REGION=ap-southeast-2 -T stats s3-to-dc "s3://dea-public-data/derivative/ga_ls_fc_3/2-5-0/088/079/2015/04/*/*.json" --no-sign-request --skip-lineage --stac ga_ls_fc_3
+docker-compose exec -e AWS_DEFAULT_REGION=ap-southeast-2 -T stats s3-to-dc "s3://dea-public-data/derivative/ga_ls_fc_3/2-5-0/088/079/2015/05/*/*.json" --no-sign-request --skip-lineage --stac ga_ls_fc_3
+
+echo "Checking WO summary EODatasets3 integration save tasks"
+docker-compose exec -T stats odc-stats save-tasks --grid=au-30 --year=2015 --overwrite ga_ls_fc_3+ga_ls_wo_3 fcp-eo3-test-run.db
+
+echo "Checking WO summary EODatasets3 integration job run"
+docker-compose exec -T stats wget https://raw.githubusercontent.com/GeoscienceAustralia/dea-config/master/dev/services/odc-stats/fc_percentile/ga_ls_fc_pc_cyear_3.yaml
+docker-compose exec -T stats odc-stats run  --threads=1 --config ga_ls_fc_pc_cyear_3.yaml --resolution=30 --location file:///tmp fcp-eo3-test-run.db 0 --apply_eodatasets3
+
+docker-compose exec -T stats ls -R /tmp/x18/4y82
