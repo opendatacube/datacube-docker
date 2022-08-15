26cce467a60e8a038ce73d8d88a3215565d93055
pin.jin@ga.gov.au <pin.jin@ga.gov.au>
2022-08-15 23:46:18 +0000

    try run-id

--- index/archival/run.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/bash
-
-set -eu
-
-for product in $(cat prods.txt); do
-    PGPASSWORD=${DB_PASSWORD} psql --no-psqlrc --echo-all --file=query.sql -v product_name=$product -h localhost -U ${DB_USERNAME} -p 5437 odc
-done
