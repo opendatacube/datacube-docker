9435899bc9f152bc485f46d46c85d0818dacfff7
Damien Ayers <damien@omad.net>
2023-10-17 14:12:33 +1100

    Work around ODC bug\n\ndatacube system init --with-any-flags doesn't work

--- index/assets/bootstrap-odc.sh
+++ index/assets/bootstrap-odc.sh
@@ -13,8 +13,16 @@ set -o nounset
 product_catalog=$1
 metadata_catalog=$2
 
-datacube system init --no-default-types --no-init-users
+# Workaround for system init bug
+#PGPASSWORD=$DB_PASSWORD psql -h "$DB_HOSTNAME" -c 'create schema agdc;' -U "$DB_USERNAME" "$DB_DATABASE"
+
+datacube system init
 # Created using : datacube metadata list | awk '{print $1}' | xargs datacube metadata show
+
+# Workaround for system init bug
+PGPASSWORD=$DB_PASSWORD psql -h "$DB_HOSTNAME" -c 'drop view agdc.dv_eo3_dataset ; drop view agdc.dv_eo_dataset ; drop view agdc.dv_telemetry_dataset ; delete from agdc.metadata_type' -U "$DB_USERNAME" "$DB_DATABASE"
+
+
 datacube metadata add "$metadata_catalog"
 dc-sync-products "$product_catalog"
 
--- index/test_bootstrapping.sh
+++ index/test_bootstrapping.sh
@@ -3,9 +3,9 @@
 # Echo lines and fail fast
 set -ex
 
-docker-compose up -d
-docker-compose run index bash -c "cd \$HOME && /code/bootstrap-odc.sh \$PRODUCT_CATALOG \$METADATA_CATALOG" || \
+docker compose up -d
+docker compose run index bash -c "cd \$HOME && /code/bootstrap-odc.sh \$PRODUCT_CATALOG \$METADATA_CATALOG" || \
   echo "WARNING: testing bootstrap script is FAILING! And we're ignoring it!"
-docker-compose exec -T postgres psql -U postgres -c "SELECT count(*) from agdc.metadata_type"
-docker-compose exec -T postgres psql -U postgres -c "SELECT count(*) from agdc.dataset_type"
-docker-compose down
+docker compose exec -T postgres psql -U postgres -c "SELECT count(*) from agdc.metadata_type"
+docker compose exec -T postgres psql -U postgres -c "SELECT count(*) from agdc.dataset_type"
+docker compose down
