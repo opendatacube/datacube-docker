e439b7aaf958cd4a1252c91ab681dc0297e3c0e4
Peter A. Jonsson <jonsson.peter.a@gmail.com>
2024-07-24 11:51:07 +0200

    index/test_*sh: run as regular user
    Stop running the tests as root.

--- index/test_bootstrapping.sh
+++ index/test_bootstrapping.sh
@@ -6,7 +6,7 @@ set -ex
 docker compose up -d
 
 # This workflow is soooo flaky. External data dependencies have broken it!
-docker compose run index bash -c "cd \$HOME && /code/bootstrap-odc.sh \$PRODUCT_CATALOG \$METADATA_CATALOG" || \
+docker compose run --user ubuntu index bash -c "cd \$HOME && /code/bootstrap-odc.sh \$PRODUCT_CATALOG \$METADATA_CATALOG" || \
   echo "WARNING: testing bootstrap script is FAILING! And we're ignoring it!"
 docker compose exec -T postgres psql -U postgres -c "SELECT count(*) from agdc.metadata_type"
 docker compose exec -T postgres psql -U postgres -c "SELECT count(*) from agdc.dataset_type"
